1fcce5b0b889ad94cdbf54244a01a34af6dbf8f7
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-09-26 18:26:55 +0000

    0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,20 +1,9 @@
-def square_matrix_simple(matrix=[]):
+#!/usr/bin/python3
+    square_matrix_simple(matrix=[]):
     nm = []
     for vec in matrix:
         sqr_vec = []
         for i in vec:
             sqr_vec.append(i**2)
         nm.append(sqr_vec)
-
     return(nm)
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
