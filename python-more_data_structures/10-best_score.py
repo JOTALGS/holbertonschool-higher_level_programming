f62eab0f1544f9759e168fbc7d0fa77fa5fa8396
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-09-27 18:39:20 +0000

    10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_val = 0
+    max_key = ""
+    for key in a_dictionary:
+        if max_val < a_dictionary[key]:
+            max_val = a_dictionary[key]
+            max_key = key
+    
+    return max_key
