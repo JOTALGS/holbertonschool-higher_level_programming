f77db6579a223a296cd394eabf7bdeed3d2258a1
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-09-27 17:54:40 +0000

    4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    interset = set()
+    for element in set_1:
+        if element not in set_2:
+            interset.add(element)
+    return (interset)
