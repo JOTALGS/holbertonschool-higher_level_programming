9cc183e3502e884b609bd506f8c4804032ec7a22
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-09-28 14:34:44 +0000

    sd

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+def main():
+    ln = len(sys.argv) - 1
+    print(ln)
+    if ln == 0:
+        print("{} arguments:".fotmat(ln))
+    elif ln == 1:
+        print("{} argument:".format(ln))
+    else:
+        print("{} arguments:".format(ln))
+    if ln > 0:
+        for i in range(1,ln + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    main()
