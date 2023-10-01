bbe57c6dcc7c5b3bda91c89f1677e737440b9054
JOTALGS <jose.pgilsuarez@gmail.com>
2023-09-30 21:40:18 -0300

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -10,7 +10,7 @@ class Square:
 
     def area(self):
         return (self.__size ** 2)
-    
+
     def my_print(self):
         for _ in range(0, self.__size):
             for _ in range(0, self.__size):
