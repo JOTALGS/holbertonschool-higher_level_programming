13b12bd086500d878eb5c5e080b84ef7998ea1b6
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-03 12:44:26 -0300

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,3 +17,27 @@ class Rectangle:
         if height< 0:
             raise ValueError("height must be >= 0")
         self.height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    def set_w(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
+    @property
+    def height(self):
+        return self.height
+
+    @height.setter
+    def set_h(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.height = value
