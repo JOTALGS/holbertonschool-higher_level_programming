3b54352ad5a91304939bfdace352b74b6a55f956
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-03 12:50:39 -0300

    1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height< 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
 
@@ -23,7 +23,7 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def set_w(self, value):
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -35,7 +35,7 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def set_h(self, value):
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
