57516ccf780c5cf97f81722a6b7d226697d70f3a
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-05 17:08:42 +0000

    jhh2

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,5 +12,5 @@ class BaseGeometry:
         """sdadfgg"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        if value <= 0:
+        if value < 1:
             raise ValueError(f"{name} must be greater than 0")
