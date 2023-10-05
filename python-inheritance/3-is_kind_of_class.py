7954b4098964f3fd76b172215771757b20ddaacb
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-05 16:19:36 +0000

    #2

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""dasdasd"""
+
+    
+def is_kind_of_class(obj, a_class):
+    """dsadsd"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
