4c041c689d017610597fc20162c4c4c021e20f4a
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-05 17:12:09 +0000

    jhh2

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """sdadfgg"""
-        if type(value) is not int:
+        if type(value) is not type(int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
