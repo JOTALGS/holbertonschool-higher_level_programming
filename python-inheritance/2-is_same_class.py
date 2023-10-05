539fe7ac15812c58e00ad02ff8de73c6ac97e73c
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-05 16:17:08 +0000

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """dasdasd"""
 
+
 def is_same_class(obj, a_class):
     """dsadsd"""
-    if isinstance(obj, a_class):
+    if type(obj) is type(a_class):
         return True
     else:
         return False
