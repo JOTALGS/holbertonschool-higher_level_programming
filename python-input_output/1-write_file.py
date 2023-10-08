77b8209971d7ac9ff2f35e76bb15bde9c9d938d0
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-08 17:21:34 -0300

    1!

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 def write_file(filename="", text="", encoding="utf-8"):
     """sds"""
-    with open(filename, "rw") as file:
+    with open(filename, "w") as file:
         st = file.write(text)
         file.close()
     return(st)
