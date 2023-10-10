df447a9da4b03564a6df9823af75820bf34f82ca
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-10 17:17:16 -0300

    hh

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,10 +10,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 def main():
     """jgvjhfhgd"""
     filn = "add_item.json"
-    if not os.path.exists(filn):
-        ls = []
-    else:
+    if os.path.exists(filn):
         ls = load_from_json_file(filn)
+    else:
+        ls = []
     for i in range(1, len(sys.argv)):
         ls.append(sys.argv[i])
     save_to_json_file(ls, filn)
