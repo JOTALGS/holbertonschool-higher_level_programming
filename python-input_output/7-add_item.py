7118ed7db2ce79829c273e4c580939ace3df55e3
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-10 17:15:11 -0300

    hh

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-
 filn = "add_item.json"
 with open(filn, "w", encoding="UTF8") as jfil:
     if not os.path.exists(filn):
