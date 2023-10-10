58b1a2e6b2ebd02702aadd7c569f42a399b29113
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-10 17:12:53 -0300

    hh

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,16 +7,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    """jgvjhfhgd"""
-    filn = "add_item.json"
-    with open(filn, "w", encoding="UTF8") as jfil:
-        if not os.path.exists(filn):
-            ls = []
-        else:
-            ls = load_from_json_file(filn)
-        for i in range(1, len(sys.argv)):
-            ls.append(sys.argv[i])
-        save_to_json_file(ls, filn)
 
-main()
+filn = "add_item.json"
+with open(filn, "w", encoding="UTF8") as jfil:
+    if not os.path.exists(filn):
+        ls = []
+    else:
+        ls = load_from_json_file(filn)
+    for i in range(1, len(sys.argv)):
+        ls.append(sys.argv[i])
+    save_to_json_file(ls, filn)
