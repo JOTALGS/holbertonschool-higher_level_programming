951127d67df9dd46a9fa6f75050ecdc17cbc5cf9
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-12 17:47:36 +0000

    9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,6 +9,13 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """fdfd"""
-        return self.__dict__
+        new_dic = {}
+        if attrs is not None:
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    new_dic[att] = self.__dict__[att]
+            return new_dic
+        else:
+            return self.__dict__
