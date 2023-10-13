090befe2e01e3546a1bacaae44e7905e36ab8a23
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-13 17:37:29 -0300

    jh

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,7 +15,6 @@ class Rectangle(Base):
         x (int)
         y (int)
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Constructor method for Rectangle class.
