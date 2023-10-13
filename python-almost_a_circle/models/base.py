a50d923dc5f70e94e3a76d7db505acc1ac4eefc7
JOTALGS <jose.pgilsuarez@gmail.com>
2023-10-13 17:53:15 -0300

    hhswd

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,18 +26,8 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ddsfvhjfvjh sdsad"""
         if list_dictionaries is not None:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
-
-
-if __name__ == "__main__":
-
-    r1 = Rectangle(10, 7, 2, 8)
-    dictionary = r1.to_dictionary()
-    json_dictionary = Base.to_json_string([dictionary])
-    print(dictionary)
-    print(type(dictionary))
-    print(json_dictionary)
-    print(type(json_dictionary))
