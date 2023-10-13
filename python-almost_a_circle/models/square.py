e3c0a77254fd30b1ab3336e1b7015cc81569da6a
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-10-13 13:43:51 +0000

    set

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,16 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        if type(val) is not int:
+            raise TypeError("width must be an integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
+        self.width = val
+        self.height = val
