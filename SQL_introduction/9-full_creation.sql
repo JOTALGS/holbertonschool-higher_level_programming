f69289a96bfd1748a9c5e1a06dd620e5e13fef10
JOTALGS <jose.pgilsuarez@gmail.com>
2023-11-15 19:46:08 -0300

    j9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
--- Create table and fill some info.
+-- Create table and fill some info safsds sad sd sad sd s dsd sad sdsd assd asd asd sad  sadsad sa dsads ds asd sads.
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score) VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
