e803e446cb2a1afb6de0411ca92e8ec3be76132a
JOTALGS <jose.pgilsuarez@gmail.com>
2023-11-15 19:12:44 -0300

    jg

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Insetr new row to table
+INSERT INTO first_table(id, name) VALUE (89, 'Best School')
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- count the number of rows with id=19
+SELECT COUNT(*) FROM first_table WHERE id = 89;
