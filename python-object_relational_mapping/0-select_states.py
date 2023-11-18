001001b3e21f4578eedc187af8843955f11bda8a
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-11-18 22:44:28 +0000

    jg

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,13 +9,19 @@ def main():
     user_name = sys.argv[1]
     user_passw = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', user=user_name, passwd=user_passw, db=db_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=user_name,
+        passwd=user_passw,
+        db=db_name
+        )
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)
     db.close()
+
 
 if __name__ == "__main__":
     main()
