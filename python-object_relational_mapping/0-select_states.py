8781f39ab93422884391d91676115895de930f99
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2023-11-18 22:34:06 +0000

    j

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""dsdsdd sds ds dsd """
+import MySQLdb
+import sys
+
+
+def main():
+    """,ain fun ca"""
+    user_name = sys.argv[1]
+    user_passw = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=user_name, passwd=user_passw, db=db_name)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY states.id')
+    rows = cur.fetchall()
+    # Print the results
+    for row in rows:
+        print(row)
+    # Close the connection
+    db.close()
+
+if __name__ == "__main__":
+    main()
