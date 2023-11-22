d9265426a2f7d0b80186d2ae3bfa689c51e89c8e
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 19:45:26 +0000

    2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""dsdsdd sds ds dsd """
+if __name__ == "__main__":
+    """,ain fun ca"""
+    import MySQLdb
+    import sys
+    user_name = sys.argv[1]
+    user_passw = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=user_passw,
+        db=db_name
+        )
+    cur = db.cursor()
+    qry = "SELECT * FROM states WHERE \
+        name LIKE {state_name} ORDER BY id ASC"
+    cur.execute(qry)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    db.close()
