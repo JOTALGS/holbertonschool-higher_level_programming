1405d465840a5de2c2d22395e61799f72853ea62
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 23:56:25 +0000

    sd

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """dsdsdd sds ds dsd """
 
-if __name__ == "__main__":
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-    Base = declarative_base()
+Base = declarative_base()
 
-    class State(Base):
-        """comne asdsds docu nadns ansdnsndjdd js dj"""
-        __tablename__ = 'states'
-        id = Column(Integer, autoincrement='auto',
-                    unique=True, nullable=False, primary_key=True)
-        name = Column(String(128), nullable=False)
+
+class State(Base):
+    """comne asdsds docu nadns ansdnsndjdd js dj"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement='auto',
+                unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
