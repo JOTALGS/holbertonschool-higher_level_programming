07428ddd3f2948c32ae899740fa645a9d550095a
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 23:49:59 +0000

    sd

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     from sqlalchemy import Column, Integer, String
 
     Base = declarative_base()
-    
+
     class State(Base):
         __tablename__ = 'states'
-        id = Column(Integer, autoincrement='auto', unique=True ,primary_key=True)
+        id = Column(Integer, autoincrement='auto',
+                    unique=True, primary_key=True)
         name = Column(String(128))
