171c94351d86112536736c1fe6642d5e5e9e5768
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 21:21:14 +0000

    2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     cur.execute(qry)
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
 
     db.close()
