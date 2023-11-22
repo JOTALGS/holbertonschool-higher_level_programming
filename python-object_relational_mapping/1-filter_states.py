f58ee3de1c29fe3ed3a3089efb0c58fe2d9abdbc
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 18:49:53 +0000

    201

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""dsdsdd sds ds dsd """
+if __name__ == "__main__":
+    """,ain fun ca"""
+    import MySQLdb
+    import sys
+    user_name = sys.argv[1]
+    user_passw = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=user_passw,
+        db=db_name
+        )
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE '%N' ORDER BY states.id ASC')
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    db.close()
