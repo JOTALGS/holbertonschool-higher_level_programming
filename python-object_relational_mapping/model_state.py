7d6f5084a33ce287c95287d5b43f018a6520a51f
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-22 23:47:50 +0000

    sd

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""dsdsdd sds ds dsd """
+
+if __name__ == "__main__":
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import Column, Integer, String
+
+    Base = declarative_base()
+    
+    class State(Base):
+        __tablename__ = 'states'
+        id = Column(Integer, autoincrement='auto', unique=True ,primary_key=True)
+        name = Column(String(128))
