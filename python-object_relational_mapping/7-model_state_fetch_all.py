2522ea2521bc122af052154d7abcc6e5feb64010
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-23 00:51:42 +0000

    fetchstates

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,23 +3,25 @@
     sadarfeafewfwet  v gr gefg f afdsf sdg feaa dsf sdf
 """
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-if __name_ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
         sys.argv[1],
         sys.argv[2],
-        sys.argv[3]
-    ))
+        sys.argv[3]),
+        pool_pre_ping=True
+    )
 
-    sess = sessionmaker(bind=engine)
-    Session = sess()
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
     session = Session()
-    result = session.query(State).order_by(State.id)
 
-    for resu in result:
-        print(resu)
+    states = session.query(State)
+    for state in states:
+        print(f"{state.id}: {state.name}")
